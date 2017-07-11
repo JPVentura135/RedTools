fe1bea53d4c6a41b3a2472e6548ab4e9e2a2ee3e
JPVentura <jp.ventura135@gmail.com>
2017-07-11 14:21:36 -0400

    fixed text wrapping to align with page margin.

--- redtools.py
+++ redtools.py
@@ -5,42 +5,46 @@ import os
 
 def  reduc_tbl():
     '''
-    This function iteratively opens and reads .fits files in a given directory and compiles a csv file containing 
-    the filename, target name, reduction variables (bias and flat field) and observation filters for each file 
+    This function iteratively opens and reads .fits files in a given directory
+    and compiles a csv file containing the filename, target name, reduction
+    variables (bias and flat field) and observation filters for each file
     (observation log).
-    
+
     ==========
     Arguments:
     ==========
-    
-    - No arguments 
-    
+
+    - No arguments
+
     ========
     Returns:
     ========
-    
-    - .CSV file containing the filename, target name, reduction variables (bias and flat field) 
-      and observation filters for each .fits file within specified directory.
+
+    - .CSV file containing the filename, target name, reduction variables
+      (bias and flat field) and observation filters for each .fits file within
+      specified directory.
     '''
 
-    #*************************************************************************************************************#
+    #**************************************************************************#
 
-    
-    # Prompt user input of directory path where files to be summarized are located.
+
+    # Prompt user input of directory path where files to be summarized are
+    # located.
 
     path = os.path.expanduser(raw_input('Please enter the path to .fits files: \n\n'))
-    
 
-    # Make sure pathname ends in '/' and if not then add on to the end of the path string. 
+
+    # Make sure pathname ends in '/' and if not then add on to the end of the
+    # path string.
 
     if path[-1] != '/':
         path + '/'
     else:
-        path = path    
-    
+        path = path
+
     print(2*'\n\n' + path + 3*'\n\n')
-	
-    
+
+
     # Create empty lists for construction of data arrays by iterative appending.
     # Initialize lists in the event of processing a large volume of files.
 
@@ -61,8 +65,9 @@ def  reduc_tbl():
                 comment  = []
 
 
-    # Open, read, and iterate over files ending with the .fits extension in inputted path.
-    # Assign fits header key data to a variable and then append iteratively to respective empty list.
+    # Open, read, and iterate over files ending with the .fits extension in
+    # inputted path. Assign fits header key data to a variable and then append
+     # iteratively to respective empty list.
 
 
     for fitsfile in os.listdir(path):
@@ -71,54 +76,55 @@ def  reduc_tbl():
                 with fits.open(os.path.join(path,fitsfile)) as hdulist:
 
                     hdr = hdulist[0]
-					
-                    # local fits file name is used here; filename list elements not uniform across format
+
+                    # Local fits file name is used here; filename list elements
+                    # not uniform across format.
 
                     filename    = hdr.header['FILENAME']
                     fileid.append(fitsfile)
                     print(fitsfile)
-                    print('\n\n') 
+                    print('\n\n')
 
                     object_name = hdr.header['OBJECT']
                     objname.append(object_name)
-                    print(object_name)                    
-                    print('\n\n') 
+                    print(object_name)
+                    print('\n\n')
 
                     obsvtyp     = hdr.header['OBSTYPE']
                     obsvtype.append(obsvtyp)
-                    print(obsvtype)             
-                    print('\n\n') 
-                    
+                    print(obsvtype)
+                    print('\n\n')
+
                     filt        = hdr.header['FILTERS']
                     filtr.append(filt)
                     print(filtr)
-                    print('\n\n')                    
-                    
+                    print('\n\n')
+
                     comment.append(' ')
-                
+
             except IOError:
                 print(fitsfile + ' is a  ** Bad file ** \n\n')
                 fileid.append(str.format(fitsfile))
                 objname.append(' N/A ')
                 obsvtype.append(' N/A ')
                 filtr.append(' N/A ')
-                comment.append('Empty or corrupt FITS file.')       # \033[01;31m : ANSI color code for red text
+                comment.append('Empty or corrupt FITS file.')
                 continue
-         
-        
+
+
     print(fileid,objname,obsvtype,filtr,comment)
 
 
-    # Create an empty pandas dataframe object (data table) and assign populated data lists to dataframe
-    # columns.
+    # Create an empty pandas dataframe object (data table) and assign populated
+    # data lists to dataframd columns.
 
 
     dataframe = pd.DataFrame(data = None)
-    dataframe['Filename']         = fileid     
-    dataframe['Object_Name']      = objname    
-    dataframe['Observation_Type'] = obsvtype   
-    dataframe['Filter']           = filtr 
-    dataframe['Comment']          = comment     
+    dataframe['Filename']         = fileid
+    dataframe['Object_Name']      = objname
+    dataframe['Observation_Type'] = obsvtype
+    dataframe['Filter']           = filtr
+    dataframe['Comment']          = comment
     print(dataframe)
 
 
